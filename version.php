03cee7545ef6a6b26bdb1bc80e4c98c3de64a1bf
Mark Sharp <mark.sharp@solent.ac.uk>
2026-04-15 15:37:58 +0100

    Version bump

--- version.php
+++ version.php
@@ -27,7 +27,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'local_sync_courseleaders';
 $plugin->release      = '1.0';
-$plugin->version      = 2025062301;
+$plugin->version      = 2026041500;
 $plugin->requires     = 2024100700;
 $plugin->supported    = [405, 500];
 $plugin->maturity     = MATURITY_STABLE;
